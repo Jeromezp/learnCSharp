e153a601bcb17b69da94088855e0f355226befdd
JeromeZhang <jeromezhang@thinkgeo.com>
2016-07-28 09:42:41 +0800

    create LearnDemoOne

---
+++ LeanDemoOne/LeanDemoOne/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeanDemoOne
+{
+    class Animal
+    {
+        public string name;
+        public string category;
+        public int age;
+
+        //overload
+        public Animal()
+        {
+
+        }
+
+        public Animal(string name,string category,int age)
+        {
+            this.name = name;
+            this.category = category;
+            this.age = age;
+        }
+
+        //override
+        override public string  ToString()
+        {
+            string info = "name: " + this.name + ",age: " + this.age + ",category: " + this.category;
+            return info;
+        }
+
+        virtual public void say()
+        {
+            Console.WriteLine("this is animal say");
+        }
+    }
+}
---
+++ LeanDemoOne/LeanDemoOne/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeanDemoOne
+{
+    class Dog : Animal
+    {
+        public override void say()
+        {
+            base.say();
+        }
+        override public string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
